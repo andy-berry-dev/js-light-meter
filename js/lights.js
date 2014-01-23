dce4e3b65841618e9f874ac3e6e58ebac9a670af
Andy Berry <andyberry88@gmail.com>
2014-01-23 20:31:47 +0000

    starting to make lights a class so there can be multiple and its more self contained

--- js/lights.js
+++ js/lights.js
@@ -1,103 +1,104 @@
 (function($, document, window) {
 	"use strict"
 
-	/* config */
-	var lights = {}
-	lights.CANVAS_ID = "lightsCanvas";
-	lights.CANVAS_WIDTH = 20;
-	lights.CANVAS_HEIGHT = 365;
-	lights.CANVAS_WRAPPER_ID = "lightsCanvasWrapper";
-	lights.NUMBER_OF_LIGHTS = 11;
-	lights.LIGHT_RADIUS = 10;
-	lights.LIGHT_PADDING = 12;
-	lights.UPDATE_INTERVAL = 100;
+	var lights = function()
+	{
+		this.CANVAS_ID = "lightsCanvas";
+		this.CANVAS_WIDTH = 20;
+		this.CANVAS_HEIGHT = 365;
+		this.CANVAS_WRAPPER_ID = "lightsCanvasWrapper";
+		this.NUMBER_OF_LIGHTS = 11;
+		this.LIGHT_RADIUS = 10;
+		this.LIGHT_PADDING = 12;
+		this.UPDATE_INTERVAL = 100;
+	
+		this.logEnabled = true;
+		
+		this.lights = new Array();
+		for (var i = 0; i < this.NUMBER_OF_LIGHTS; i++) {
+			var thisLightX = this.CANVAS_WIDTH / 2;
+			var thisLightY = ( i * this.LIGHT_PADDING) + ( (i+1) * this.LIGHT_RADIUS * 2);
+			var lightColour;
+	
+			switch(i) {
+				case 0:
+				case 1:
+				case 2:
+				case 3:
+					lightColour = "green";
+					break;
+				case 4:
+				case 5:
+				case 6:
+				case 7:
+					lightColour = "orange";
+					break;
+				case 8:
+				case 9:
+				case 10:
+					lightColour = "red";
+					break;
+			}
 
-	/* end config */
-
-	lights.lights = new Array();
-	for (var i = 0; i < lights.NUMBER_OF_LIGHTS; i++) {
-		var thisLightX = lights.CANVAS_WIDTH / 2;
-		var thisLightY = ( i * lights.LIGHT_PADDING) + ( (i+1) * lights.LIGHT_RADIUS * 2);
-		var lightColour;
-
-		switch(i) {
-			case 0:
-			case 1:
-			case 2:
-			case 3:
-				lightColour = "green";
-				break;
-			case 4:
-			case 5:
-			case 6:
-			case 7:
-				lightColour = "orange";
-				break;
-			case 8:
-			case 9:
-			case 10:
-				lightColour = "red";
-				break;
+			this.lights[this.NUMBER_OF_LIGHTS - i - 1] = {
+				"colour" : lightColour,
+				"x" : thisLightX,
+				"y" : thisLightY,
+				"radius" : this.LIGHT_RADIUS,
+				"strokeStyle" : "#000",
+				"state" : "off"
+			}
 		}
 
-		lights.lights[lights.NUMBER_OF_LIGHTS - i - 1] = {
-			"colour" : lightColour,
-			"x" : thisLightX,
-			"y" : thisLightY,
-			"radius" : lights.LIGHT_RADIUS,
-			"strokeStyle" : "#000",
-			"state" : "off"
-		}
+		this.queue = new Array();
+		this.interval = -1;
+		this.endOfQueueValue = -1;
 	}
-
-	lights.queue = new Array();
-	lights.interval = -1;
-	lights.endOfQueueValue = -1;
-	lights._processQueue = function() {
-		if (lights.queue.length > 0) {
-			var method = lights.queue.shift();
+	lights.prototype._processQueue = function() {
+		if (this.queue.length > 0) {
+			var method = this.queue.shift();
 			if (method) {
-				method();
-				lights.draw();
+				method.bind(this).call();
+				this.draw();
 			}
 		} else {
-			lights._stopProcessing();
+			this._stopProcessing();
 		}
 	}
-	lights._startProcessing = function() {
-		lights.draw();
-		if (lights.interval == -1) {
-			lights.interval = setInterval(lights._processQueue, lights.UPDATE_INTERVAL);
+	lights.prototype._startProcessing = function() {
+		this.draw();
+		if (this.interval == -1) {
+			this.interval = setInterval(this._processQueue.bind(this), this.UPDATE_INTERVAL);
 		}
 	}
-	lights._stopProcessing = function() {
-		if (lights.interval != -1) {
-			clearInterval(lights.interval)
-			lights.interval = -1;
+	lights.prototype._stopProcessing = function() {
+		if (this.interval != -1) {
+			clearInterval(this.interval)
+			this.interval = -1;
 		}
 	}
-	lights._pushMethod = function(fn, params) {
-		lights.queue.push( function() {
-	    	fn.apply(window, params);
+	lights.prototype._pushMethod = function(fn, params) {
+		this.queue.push( function() {
+	    	fn.bind(this).apply(window, params);
 		});
-		lights._startProcessing();
+		this._startProcessing();
 	}
-	lights._setValue = function(value) {
+	lights.prototype._setValue = function(value) {
 		var theValue = value || 0;
 		theValue = (theValue < 0) ? 0 : theValue;
-		for (var lightNum = 0; lightNum < lights.lights.length; lightNum++) {
-			var light = lights.lights[lightNum];
+		for (var lightNum = 0; lightNum < this.lights.length; lightNum++) {
+			var light = this.lights[lightNum];
 			light.state = (lightNum < value) ? "on" : "off";
 		}
 	}
-	lights._setEndOfQueueValue = function(val) {
-		lights.endOfQueueValue = val;
+	lights.prototype._setEndOfQueueValue = function(val) {
+		this.endOfQueueValue = val;
 	}
-	lights.draw = function() {
-		lights.canvasElement.clearCanvas();
-		for (var lightNum = 0; lightNum < lights.lights.length; lightNum++) {
-			var light = lights.lights[lightNum];
-			lights.canvasElement.drawArc({
+	lights.prototype.draw = function() {
+		this.canvasElement.clearCanvas();
+		for (var lightNum = 0; lightNum < this.lights.length; lightNum++) {
+			var light = this.lights[lightNum];
+			this.canvasElement.drawArc({
 				strokeStyle: light.strokeStyle,
 				fillStyle: (light.state == "on") ? light.colour : "none",
 				x: light.x, 
@@ -106,79 +107,77 @@
 			})
 		}
 	}
-	lights._noop = function() { }
+	lights.prototype._noop = function() { }
 
-
-
-	lights.setInterval = function(interval) {
-		lights._pushMethod(function() {
-			lights.UPDATE_INTERVAL = interval;
-			lights._stopProcessing();
-			lights._startProcessing();
+	lights.prototype.setInterval = function(interval) {
+		this._pushMethod(function() {
+			this.UPDATE_INTERVAL = interval;
+			this._stopProcessing();
+			this._startProcessing();
 		}, []);
 	}
-	lights.getInterval = function() {
-		return lights.UPDATE_INTERVAL;
+	lights.prototype.getInterval = function() {
+		return this.UPDATE_INTERVAL;
 	}
-	lights.pauseFor = function(val) {
+	lights.prototype.pauseFor = function(val) {
 		for (var i = 0; i < val; i++) {
-			lights._pushMethod(lights._noop, []);
+			this._pushMethod(this._noop, []);
 		}
 	}
-	lights.flashUpTo = function(start, val) {
-		var limit = val || lights.NUMBER_OF_LIGHTS;
+	lights.prototype.flashUpTo = function(start, val) {
+		var limit = val || this.NUMBER_OF_LIGHTS;
 		var startVal = start || 0;
 		for (var lightNum = startVal; lightNum <= limit; lightNum++) {
-			lights._pushMethod( lights._setValue, [lightNum]);
+			this._pushMethod( this._setValue, [lightNum]);
 		}
-		lights._setEndOfQueueValue(limit);
+		this._setEndOfQueueValue(limit);
 	}
-	lights.flashDownTo = function(start, val) {
+	lights.prototype.flashDownTo = function(start, val) {
 		var limit = (val || 0);
-		var startVal = (start!=undefined) ? start : lights.NUMBER_OF_LIGHTS -1;
+		var startVal = (start!=undefined) ? start : this.NUMBER_OF_LIGHTS -1;
 		for (var lightNum = startVal; lightNum >= limit; lightNum--) {
-			lights._pushMethod( lights._setValue, [lightNum]);
+			this._pushMethod( this._setValue, [lightNum]);
 		}
-		lights._setEndOfQueueValue(limit);
+		this._setEndOfQueueValue(limit);
 	}
-	lights.flashTo = function(start, val) {
+	lights.prototype.flashTo = function(start, val) {
 		if (val > start) {
-			lights.flashUpTo(start, val);
+			this.flashUpTo(start, val);
 		} else {
-			lights.flashDownTo(start, val);
+			this.flashDownTo(start, val);
 		}
 	}
-	lights.changeValueBy = function(diff) {
-		var curValue = lights.getValue();
+	lights.prototype.changeValueBy = function(diff) {
+		var curValue = this.getValue();
 		var newValue = curValue + diff;
-		lights.flashTo(curValue, newValue);
+		this.flashTo(curValue, newValue);
 	}
-	lights.getValue = function() {
-		return lights.endOfQueueValue;
+	lights.prototype.getValue = function() {
+		return this.endOfQueueValue;
 	}
-	lights.blinkLight = function(count) {
+	lights.prototype.blinkLight = function(count) {
 		var blinkCount = count || 5;
 		for (var i = 0; i < blinkCount; i++) {
-			lights.changeValueBy(-1);
-			lights.changeValueBy(1);
+			this.changeValueBy(-1);
+			this.changeValueBy(1);
 		}
 	}
 
-	lights.init = function() {
-		$("<canvas id='"+lights.CANVAS_ID+"' width='"+lights.CANVAS_WIDTH+
-			"' height='"+lights.CANVAS_HEIGHT+"'></canvas>")
-			.appendTo("#"+lights.CANVAS_WRAPPER_ID);
-		lights.canvasElement = $("#"+lights.CANVAS_ID);
+	lights.prototype.init = function() {
+		$("<canvas id='"+this.CANVAS_ID+"' width='"+this.CANVAS_WIDTH+
+			"' height='"+this.CANVAS_HEIGHT+"'></canvas>")
+			.appendTo("#"+this.CANVAS_WRAPPER_ID);
+		this.canvasElement = $("#"+this.CANVAS_ID);
 
-		lights._startProcessing();
-		lights.pauseFor(2);
-		var oldInterval = lights.getInterval();
-		lights.setInterval(30);
-		lights.flashUpTo();
-		lights.flashDownTo();
-		lights.setInterval(oldInterval);
+		this._startProcessing();
+		this.pauseFor(2);
+		var oldInterval = this.getInterval();
+		this.setInterval(30);
+		this.flashUpTo();
+		this.flashDownTo();
+		this.setInterval(oldInterval);
 	}
 
-	window.lights = lights;
+	window.lights = new lights();
 	
 })(jQuery, document, window);
